771dc3616470a521e09e58ee69c6802be16906dd
Anna Kemp <anna.kemp@ratequote.com>
2023-03-02 13:37:33 -0500

    use xpath instead of class based query selectors

--- get-search-results.js
+++ get-search-results.js
@@ -12,28 +12,25 @@ async function getOrgSearchResults (query) {
   const encodedQuery = encodeURIComponent(query)
   await page.goto(`https://www.google.com/search?q=${encodedQuery}`)
 
-  const locator = page.locator('#search')
+  const locator = page.locator("xpath=//div[@id='search']")
   const searchResults = await locator.evaluate(searchContainer => {
-      const data = [];
-      const anchors = searchContainer.querySelectorAll('a:not(.WlydOe):not(.jRKCUd):not(a[href*="/search"])')
-      anchors.forEach(a => {
-        const headline = a.querySelector('h3, [role="heading"]')?.innerText
+     const data = [];
+     const container = document.evaluate(".//a[descendant::h3]/ancestor::div[position() = 3]", searchContainer)
+     let node = null
+
+      while (node = container.iterateNext()) {
+        const { stringValue: headline} = document.evaluate(".//a/h3/text()", node, null, XPathResult.STRING_TYPE, null )
         // no headline indicates it's not the type of link we want
         if (!headline) return
 
-        const parent = a.closest('.kvH3mc.BToiNc.UK95Uc')
-        const timeframe = parent?.querySelector('.MUxGbd.wuQ4Ob.WZ8Tjf')?.querySelector('span').innerText
+        const { stringValue: timeframe } = document.evaluate(".//span[contains(text(), 'min ago') or contains(text(), 'mins ago') or contains(text(), 'hour ago') or contains(text(), 'hours ago') or contains(text(), 'day ago') or contains(text(), 'days ago')]/text()", node, null, XPathResult.STRING_TYPE, null)
 
-        const regex = new RegExp(/hours|day/, 'g')
-        const timeframeIsRecent = regex.test(timeframe)
-        // no recent timeframe indicates it's not the type of link we want
-        if (!timeframeIsRecent) return
+        const { stringValue: href} = document.evaluate(".//a/@href", node, null, XPathResult.STRING_TYPE, null )
+        const { stringValue: description} = document.evaluate("string(.//span[descendant::em])", node, null, XPathResult.STRING_TYPE, null )
 
-        const href = a.href
-        const description = parent?.querySelector('em')?.closest('span').innerText
-        
-        data.push({ description, headline, href, timeframe })
-      })
+        // no timeframe indicates it's not the type of link we want
+        if (timeframe) data.push({ description, headline, href, timeframe})
+      }
       return data
   })
   await browser.close()
--- get-top-news.js
+++ get-top-news.js
@@ -12,22 +12,20 @@ async function getTopStories (query) {
   const encodedQuery = encodeURIComponent(query)
   await page.goto(`https://www.google.com/search?q=${encodedQuery}`)
 
-  const locator = page.locator('.WlydOe')
-  const newsArticles = await locator.evaluateAll((all_items) => {
+  const locator = page.locator("xpath=//g-section-with-header[descendant::span[contains(text(),'Top stories')]]//a")
+
+  const newsArticles = await locator.evaluateAll((a) => {
     {
       const data = [];
 
-      all_items.forEach(newsItem => {
-        const headline = newsItem.querySelector('.mCBkyc')?.innerText
+      a.forEach(newsItem => {
+        const headline = newsItem.querySelector('[role="heading"]')?.innerText
         const href = newsItem.href
 
-        const timeframe = newsItem.querySelector('.OSrXXb')?.innerText
-        const regex = new RegExp(/hours|day/, 'g')
-        const timeframeIsRecent = regex.test(timeframe)
-        // no recent timeframe indicates it's not the type of link we want
-        if (!timeframeIsRecent) return
+        const { stringValue: timeframe } = document.evaluate(".//span[contains(text(), 'min ago') or contains(text(), 'mins ago') or contains(text(), 'hour ago') or contains(text(), 'hours ago') or contains(text(), 'day ago') or contains(text(), 'days ago')]/text()", newsItem, null, XPathResult.STRING_TYPE, null)
 
-        data.push({headline, href, timeframe})
+        // no timeframe indicates it's not the type of link we want
+        if (timeframe) data.push({headline, href, timeframe})
       })
       return data
   }
